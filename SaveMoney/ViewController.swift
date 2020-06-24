474248e928416a5abbfaea969a741d9d760486a7
kramarkirill <kkramar@kantiana.ru>
2020-06-24 16:34:00 +0200

    Initial Commit

---
+++ SaveMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveMoney
+//
+//  Created by Кирилл Крамар on 24.06.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
